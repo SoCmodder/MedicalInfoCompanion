d51960fc5ad3f122162243381418441b6cd7b370
Mitch Miller <socmodder@gmail.com>
2019-10-04 22:39:22 -0400

    add initial image crop stuff

---
+++ app/src/main/java/world/mitchmiller/medicalinfo/util/Constants.kt
@@ -0,0 +1,7 @@
+package world.mitchmiller.medicalinfo.util
+
+class Constants {
+    companion object {
+        const val IMAGE_CROP_REQUEST = 1337
+    }
+}
--- app/src/main/java/world/mitchmiller/medicalinfo/util/FileUtils.kt
+++ app/src/main/java/world/mitchmiller/medicalinfo/util/FileUtils.kt
@@ -1,5 +1,42 @@
 package world.mitchmiller.medicalinfo.util
 
+import android.graphics.Bitmap
+import android.os.Environment
+import android.util.Log
+import java.io.File
+import java.io.FileOutputStream
+import java.lang.Exception
+import java.util.*
+
 object FileUtils {
+
+    public fun createDirectoryAndSaveBitmapFile(imageToSave: Bitmap, filename: String) {
+        val dirName = "/medical_info_companion"
+        val dir = File(Environment.getExternalStorageDirectory().path + dirName)
+
+        if (!dir.exists()) {
+            val medInfoDir = File("/sdcard"+dirName)
+            medInfoDir.mkdirs()
+        }
+
+        val file = File("/sdcard"+dirName, filename)
+        if (file.exists()) {
+            file.delete()
+        }
+        try {
+            val out = FileOutputStream(file)
+            imageToSave.compress(Bitmap.CompressFormat.JPEG, 100, out)
+            out.flush()
+            out.close()
+        } catch (e: Exception) {
+            Log.e("FileUtils", e.message)
+        }
+    }
+
+    public fun createImageFileName(): String {
+        val randomMathVal = Math.random()
+        val randomCalVal = Calendar.getInstance().get(Calendar.MILLISECOND)
+        return "doc_image" + randomMathVal.toString() + randomCalVal.toString()
+    }
 
 }
